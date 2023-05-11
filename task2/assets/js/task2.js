3f3fef124dcc5c568a1d6cde03d701807e6c2c8a
DinaraDervel <dinaradervel@gmail.com>
2023-05-10 20:17:54 -0600

    add task2

---
+++ task2/assets/js/task2.js
@@ -0,0 +1,5 @@
+let image = document.getElementById("dog-image");
+      let showPreviousImg = () =>
+        (image.src = "assets/images/german-shepherd.jpg");
+      let showNextImg = () =>
+        (image.src = "assets/images/golden-retriever.jpg");
